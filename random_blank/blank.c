0917b07f41f78d5912f81cd7da0131e83c417d4d
Max Tsai <b38241@freescale.com>
2015-07-13 17:19:55 +0800

    [random_blank] add longer blank field.

--- random_blank/blank.c
+++ random_blank/blank.c
@@ -9,7 +9,7 @@ void main(int argc, char **argv)
 {
 	FILE *fp;
 	char tmp[1024];
-	char tmp1[1024];
+	char tmp1[2048];
 	int blank_num = 0;
 
 	srand(time(NULL));
@@ -26,14 +26,18 @@ void main(int argc, char **argv)
 
 	printf("%s\n", argv[1]);
 	while(fgets(tmp, 1024, fp) != NULL) {
-		memcpy(tmp1, tmp, 1024);
-		//printf("%s\n", tmp1);
+		memset(tmp1, 0, 2048);
+		//printf("%s\n", tmp);
 		int i, j, k;
 		i = rand() % SKIP;
 		j = 1;
 
 		char *ptr = strtok(tmp, " ");
+		strcat(tmp1, ptr);
+		strcat(tmp1, " ");
+
 		while((ptr = strtok(NULL, " ")) != NULL) {
+
 			if (strlen(ptr)<2)
 				continue;
 			if (ptr[0] < 0x41 || ptr[0] > 0x7a)
@@ -41,12 +45,21 @@ void main(int argc, char **argv)
 			if (ptr[1] < 0x61 && ptr[1] > 0x5a)
 				continue;
 
+
 			if (j == i) {
-				for (k = 0; k < strlen(ptr); k++)
-					tmp1[ptr+k-tmp] = '_';
+				for (k = 0; k < strlen(ptr); k++) {
+					if (k < 5)
+						strcat(tmp1, "__");
+					else
+						strcat(tmp1, "_");
+				}
+				strcat(tmp1, " ");
 				i = rand() % SKIP;
 				j = 0;
 				blank_num++;
+			} else {
+				strcat(tmp1, ptr);
+				strcat(tmp1, " ");
 			}
 			j = (j+1) % SKIP;
 		}
