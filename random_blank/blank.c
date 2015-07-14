f6a7a938325d31ddd03bbe67c8622063a92e44ec
Max Tsai <b38241@freescale.com>
2015-07-14 14:03:12 +0800

    [random_blank]

--- random_blank/blank.c
+++ random_blank/blank.c
@@ -11,23 +11,28 @@ void main(int argc, char **argv)
 	char tmp[1024];
 	char tmp1[2048];
 	int blank_num = 0;
+	int SKIP = 10;
+	int total = 0;
 
 	srand(time(NULL));
 
-	if (argc != 2)
+	if (argc < 2) {
+		printf("usage: a.out input_file [SKIP]\n");
 		return;
+	}
+	if (argc == 3)
+		SKIP = atoi(argv[2]);
 
 	fp = fopen(argv[1], "r");
 	if (fp == NULL) {
 		printf("%s doesn't exist\n", argv[1]);
 		return;
 	}
-#define SKIP 8
 
 	printf("%s\n", argv[1]);
+
 	while(fgets(tmp, 1024, fp) != NULL) {
 		memset(tmp1, 0, 2048);
-		//printf("%s\n", tmp);
 		int i, j, k;
 		i = rand() % SKIP;
 		j = 1;
@@ -38,13 +43,15 @@ void main(int argc, char **argv)
 
 		while((ptr = strtok(NULL, " ")) != NULL) {
 
-			if (strlen(ptr)<2)
+			if ((strlen(ptr)<2) || 
+				(ptr[0] < 0x41 || ptr[0] > 0x7a) ||
+				(ptr[1] < 0x61 && ptr[1] > 0x5a)) {
+				strcat(tmp1, ptr);
+				strcat(tmp1, " ");
 				continue;
-			if (ptr[0] < 0x41 || ptr[0] > 0x7a)
-				continue;
-			if (ptr[1] < 0x61 && ptr[1] > 0x5a)
-				continue;
+			}
 
+			total++;
 
 			if (j == i) {
 				for (k = 0; k < strlen(ptr); k++) {
@@ -54,7 +61,11 @@ void main(int argc, char **argv)
 						strcat(tmp1, "_");
 				}
 				strcat(tmp1, " ");
-				i = rand() % SKIP;
+				for (k = 0; k < 10; k++) {
+					i = rand() % SKIP;
+					if (i > 3)
+						break;
+				}
 				j = 0;
 				blank_num++;
 			} else {
@@ -65,8 +76,7 @@ void main(int argc, char **argv)
 		}
 		printf("%s", tmp1);
 	}
-	printf("blank = %d\n", blank_num);
-
+	printf("\nblank/total = %d/%d\n", blank_num, total);
 err0:
 	fclose(fp);
 
